b646183b3ecd81bcb5823fa1da112fa3c0e767f5
songhuixiang <songhuixiang@hulai.com>
2021-09-07 18:01:16 +0800

    重构类型声明

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,61 +1,45 @@
 /**
- * Creates a debounced function that delays invoking `func` until after `wait`
- * milliseconds have elapsed since the last time the debounced function was
- * invoked, or until the next browser frame is drawn. The debounced function
- * comes with a `cancel` method to cancel delayed `func` invocations and a
- * `flush` method to immediately invoke them. Provide `options` to indicate
- * whether `func` should be invoked on the leading and/or trailing edge of the
- * `wait` timeout. The `func` is invoked with the last arguments provided to the
- * debounced function. Subsequent calls to the debounced function return the
- * result of the last `func` invocation.
+ * Creates a debounced function that delays invoking func until after wait milliseconds have elapsed since
+ * the last time the debounced function was invoked. The debounced function comes with a cancel method to
+ * cancel delayed invocations and a flush method to immediately invoke them. Provide an options object to
+ * indicate that func should be invoked on the leading and/or trailing edge of the wait timeout. Subsequent
+ * calls to the debounced function return the result of the last func invocation.
  *
- * **Note:** If `leading` and `trailing` options are `true`, `func` is
- * invoked on the trailing edge of the timeout only if the debounced function
- * is invoked more than once during the `wait` timeout.
+ * Note: If leading and trailing options are true, func is invoked on the trailing edge of the timeout only
+ * if the the debounced function is invoked more than once during the wait timeout.
  *
- * If `wait` is `0` and `leading` is `false`, `func` invocation is deferred
- * until the next tick, similar to `setTimeout` with a timeout of `0`.
+ * See David Corbacho’s article for details over the differences between _.debounce and _.throttle.
  *
- * If `wait` is omitted in an environment with `requestAnimationFrame`, `func`
- * invocation will be deferred until the next frame is drawn (typically about
- * 16ms).
- *
- * See [David Corbacho's article](https://css-tricks.com/debouncing-throttling-explained-examples/)
- * for details over the differences between `debounce` and `throttle`.
- *
- * @category Function
- * @param {Function} func The function to debounce.
- * @param {number} [wait=100]
- *  The number of milliseconds to delay; if omitted, `requestAnimationFrame` is
- *  used (if available).
- * @param {Object} [options={}] The options object.
- * @param {boolean} [options.leading=false]
- *  Specify invoking on the leading edge of the timeout.
- * @param {number} [options.maxWait]
- *  The maximum time `func` is allowed to be delayed before it's invoked.
- * @param {boolean} [options.trailing=true]
- *  Specify invoking on the trailing edge of the timeout.
- * @returns {Function} Returns the new debounced function.
+ * @param func The function to debounce.
+ * @param wait The number of milliseconds to delay. default value is 100.
+ * @param options The options object.
+ * @param options.leading Specify invoking on the leading edge of the timeout.
+ * @param options.maxWait The maximum time func is allowed to be delayed before it’s invoked.
+ * @param options.trailing Specify invoking on the trailing edge of the timeout.
+ * @return Returns the new debounced function.
  */
-export function debounce<T extends Function>(func: T, wait: number = 100, options?: { leading?: boolean; trailing?: boolean; maxWait?: number }) {
-    let lastArgs: any, lastThis: any, maxWait: number, result: any, timerId: NodeJS.Timeout | undefined, lastCallTime: number | undefined;
+export function debounce<T extends (...args: any) => any>(func: T, wait: number = 100, options?: DebounceSettings): DebouncedFunc<T> {
+    let lastArgs: any,
+        lastThis: any,
+        maxWait: number,
+        result: any,
+        timerId: NodeJS.Timeout | undefined,
+        lastCallTime: number | undefined,
+        lastInvokeTime = 0,
+        leading = false,
+        maxing = false,
+        trailing = true;
 
-    let lastInvokeTime = 0;
-    let leading = false;
-    let maxing = false;
-    let trailing = true;
-
-    wait = +wait || 0;
     if (options) {
         leading = !!options.leading;
         maxing = "maxWait" in options;
-        maxWait = maxing ? Math.max(+options.maxWait! || 0, wait) : 0;
+        maxWait = maxing ? Math.max(options.maxWait!, wait) : wait;
         trailing = "trailing" in options ? !!options.trailing : trailing;
     }
 
     function invokeFunc(time: number) {
-        const args = lastArgs;
-        const thisArg = lastThis;
+        let args = lastArgs,
+            thisArg = lastThis;
 
         lastArgs = lastThis = undefined;
         lastInvokeTime = time;
@@ -63,34 +47,26 @@ export function debounce<T extends Function>(func: T, wait: number = 100, option
         return result;
     }
 
-    function startTimer(pendingFunc: () => void, wait: number) {
-        return setTimeout(pendingFunc, wait);
-    }
-
-    function cancelTimer(id: NodeJS.Timeout) {
-        clearTimeout(id);
-    }
-
     function leadingEdge(time: number) {
         // Reset any `maxWait` timer.
         lastInvokeTime = time;
         // Start the timer for the trailing edge.
-        timerId = startTimer(timerExpired, wait);
+        timerId = setTimeout(timerExpired, wait);
         // Invoke the leading edge.
         return leading ? invokeFunc(time) : result;
     }
 
     function remainingWait(time: number) {
-        const timeSinceLastCall = time - (lastCallTime ?? 0);
-        const timeSinceLastInvoke = time - lastInvokeTime;
-        const timeWaiting = wait - timeSinceLastCall;
+        let timeSinceLastCall = time - (lastCallTime ?? 0),
+            timeSinceLastInvoke = time - lastInvokeTime,
+            timeWaiting = wait - timeSinceLastCall;
 
         return maxing ? Math.min(timeWaiting, maxWait - timeSinceLastInvoke) : timeWaiting;
     }
 
     function shouldInvoke(time: number) {
-        const timeSinceLastCall = time - (lastCallTime ?? 0);
-        const timeSinceLastInvoke = time - lastInvokeTime;
+        let timeSinceLastCall = time - (lastCallTime ?? 0),
+            timeSinceLastInvoke = time - lastInvokeTime;
 
         // Either this is the first call, activity has stopped and we're at the
         // trailing edge, the system time has gone backwards and we're treating
@@ -99,12 +75,12 @@ export function debounce<T extends Function>(func: T, wait: number = 100, option
     }
 
     function timerExpired() {
-        const time = Date.now();
+        let time = Date.now();
         if (shouldInvoke(time)) {
             return trailingEdge(time);
         }
         // Restart the timer.
-        timerId = startTimer(timerExpired, remainingWait(time));
+        timerId = setTimeout(timerExpired, remainingWait(time));
     }
 
     function trailingEdge(time: number) {
@@ -121,7 +97,7 @@ export function debounce<T extends Function>(func: T, wait: number = 100, option
 
     function cancel() {
         if (timerId !== undefined) {
-            cancelTimer(timerId);
+            clearTimeout(timerId);
         }
         lastInvokeTime = 0;
         lastArgs = lastCallTime = lastThis = timerId = undefined;
@@ -131,15 +107,11 @@ export function debounce<T extends Function>(func: T, wait: number = 100, option
         return timerId === undefined ? result : trailingEdge(Date.now());
     }
 
-    function pending() {
-        return timerId !== undefined;
-    }
+    function debounced() {
+        let time = Date.now(),
+            isInvoking = shouldInvoke(time);
 
-    function debounced(...args: any[]) {
-        const time = Date.now();
-        const isInvoking = shouldInvoke(time);
-
-        lastArgs = args;
+        lastArgs = arguments;
         lastThis = this;
         lastCallTime = time;
 
@@ -149,17 +121,50 @@ export function debounce<T extends Function>(func: T, wait: number = 100, option
             }
             if (maxing) {
                 // Handle invocations in a tight loop.
-                timerId = startTimer(timerExpired, wait);
+                clearTimeout(timerId);
+                timerId = setTimeout(timerExpired, wait);
                 return invokeFunc(lastCallTime);
             }
         }
         if (timerId === undefined) {
-            timerId = startTimer(timerExpired, wait);
+            timerId = setTimeout(timerExpired, wait);
         }
         return result;
     }
     debounced.cancel = cancel;
     debounced.flush = flush;
-    debounced.pending = pending;
-    return debounced as unknown as T & { cancel: () => void; flush: () => void; pending: () => boolean };
+    return debounced;
+}
+
+export interface DebounceSettings {
+    leading?: boolean | undefined;
+    maxWait?: number | undefined;
+    trailing?: boolean | undefined;
+}
+
+export interface DebouncedFunc<T extends (...args: any[]) => any> {
+    /**
+     * Call the original function, but applying the debounce rules.
+     *
+     * If the debounced function can be run immediately, this calls it and returns its return
+     * value.
+     *
+     * Otherwise, it returns the return value of the last invocation, or undefined if the debounced
+     * function was not invoked yet.
+     */
+    (...args: Parameters<T>): ReturnType<T> | undefined;
+
+    /**
+     * Throw away any pending invocation of the debounced function.
+     */
+    cancel(): void;
+
+    /**
+     * If there is a pending invocation of the debounced function, invoke it immediately and return
+     * its return value.
+     *
+     * Otherwise, return the value from the last invocation, or undefined if the debounced function
+     * was never invoked.
+     */
+    flush(): ReturnType<T> | undefined;
 }
--- src/throttle.ts
+++ src/throttle.ts
@@ -1,53 +1,37 @@
-import { debounce } from "./debounce";
-
+import { debounce, DebouncedFunc } from "./debounce";
 /**
- * Creates a throttled function that only invokes `func` at most once per
- * every `wait` milliseconds (or once per browser frame). The throttled function
- * comes with a `cancel` method to cancel delayed `func` invocations and a
- * `flush` method to immediately invoke them. Provide `options` to indicate
- * whether `func` should be invoked on the leading and/or trailing edge of the
- * `wait` timeout. The `func` is invoked with the last arguments provided to the
- * throttled function. Subsequent calls to the throttled function return the
- * result of the last `func` invocation.
+ * Creates a throttled function that only invokes func at most once per every wait milliseconds. The throttled
+ * function comes with a cancel method to cancel delayed invocations and a flush method to immediately invoke
+ * them. Provide an options object to indicate that func should be invoked on the leading and/or trailing edge
+ * of the wait timeout. Subsequent calls to the throttled function return the result of the last func call.
  *
- * **Note:** If `leading` and `trailing` options are `true`, `func` is
- * invoked on the trailing edge of the timeout only if the throttled function
- * is invoked more than once during the `wait` timeout.
+ * Note: If leading and trailing options are true, func is invoked on the trailing edge of the timeout only if
+ * the the throttled function is invoked more than once during the wait timeout.
  *
- * If `wait` is `0` and `leading` is `false`, `func` invocation is deferred
- * until the next tick, similar to `setTimeout` with a timeout of `0`.
- *
- * If `wait` is omitted in an environment with `requestAnimationFrame`, `func`
- * invocation will be deferred until the next frame is drawn (typically about
- * 16ms).
- *
- * See [David Corbacho's article](https://css-tricks.com/debouncing-throttling-explained-examples/)
- * for details over the differences between `throttle` and `debounce`.
- *
- * @category Function
- * @param {Function} func The function to throttle.
- * @param {number} [wait=100]
- *  The number of milliseconds to throttle invocations to; if omitted,
- *  `requestAnimationFrame` is used (if available).
- * @param {Object} [options={}] The options object.
- * @param {boolean} [options.leading=true]
- *  Specify invoking on the leading edge of the timeout.
- * @param {boolean} [options.trailing=true]
- *  Specify invoking on the trailing edge of the timeout.
- * @returns {Function} Returns the new throttled function.
- * @example
+ * @param func The function to throttle.
+ * @param wait The number of milliseconds to throttle invocations to. default value is 100.
+ * @param options The options object.
+ * @param options.leading Specify invoking on the leading edge of the timeout.
+ * @param options.trailing Specify invoking on the trailing edge of the timeout.
+ * @return Returns the new throttled function.
  */
-export function throttle<T extends Function>(func: T, wait: number = 100, options?: { leading?: boolean; trailing?: boolean }) {
-    let leading = true;
-    let trailing = true;
+export function throttle<T extends (...args: any) => any>(func: T, wait: number = 100, options?: ThrottleSettings): DebouncedFunc<T> {
+    let leading = true,
+        trailing = true;
 
     if (options) {
         leading = "leading" in options ? !!options.leading : leading;
         trailing = "trailing" in options ? !!options.trailing : trailing;
     }
+
     return debounce(func, wait, {
-        leading,
-        trailing,
+        leading: leading,
         maxWait: wait,
+        trailing: trailing,
     });
+}
+
+export interface ThrottleSettings {
+    leading?: boolean | undefined;
+    trailing?: boolean | undefined;
 }
