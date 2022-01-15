9711d8d453912b7c31e77ccb9efe449e3b641c6c
songhuixiang <songhuixiang@hulai.com>
2022-01-15 09:55:49 +0800

    DeepNonNullable

--- src/types.ts
+++ src/types.ts
@@ -1,32 +1,12 @@
-/**
- * Type representing primitive types in JavaScript, and thus TypeScript: string | number | bigint | boolean | symbol | null | undefined
- */
 export type Primitive = string | number | bigint | boolean | symbol | null | undefined;
 export const isPrimitive = (val: unknown): val is Primitive => {
-    if (val === null || val === undefined) {
-        return true;
-    }
-    switch (typeof val) {
-        case "string":
-        case "number":
-        case "bigint":
-        case "boolean":
-        case "symbol":
-            return true;
-        default:
-            return false;
-    }
+    if (val === null || val === undefined) return true;
+    return ["string", "number", "bigint", "boolean", "symbol"].includes(typeof val);
 };
 
-/**
- * Type representing falsy values in TypeScript: false | "" | 0 | null | undefined
- */
 export type Falsy = false | "" | 0 | null | undefined;
 export const isFalsy = (val: unknown): val is Falsy => !val;
 
-/**
- * Type representing nullish values in TypeScript: null | undefined
- */
 export type Nullish = null | undefined;
 export const isNullish = (val: unknown): val is Nullish => val == null;
 
@@ -34,9 +14,18 @@ export type DeepReadonly<T> = T extends ((...args: any[]) => any) | Primitive ? 
 interface _DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
 type _DeepReadonlyObject<T> = { readonly [P in keyof T]: DeepReadonly<T[P]> };
 
-export type DeepRequired<T> = {
-    [P in keyof T]-?: T[P] extends object | undefined ? DeepRequired<T[P]> : T[P];
-};
+export type DeepNonNullable<T> = T extends (...args: any[]) => any ? T : T extends any[] ? _DeepNonNullableArray<T[number]> : T extends object ? _DeepNonNullableObject<T> : T;
+export interface _DeepNonNullableArray<T> extends Array<DeepNonNullable<NonNullable<T>>> {}
+export type _DeepNonNullableObject<T> = { [P in keyof T]-?: DeepNonNullable<NonNullable<T[P]>> };
+
+export type DeepRequired<T> = T extends (...args: any[]) => any ? T : T extends any[] ? _DeepRequiredArray<T[number]> : T extends object ? _DeepRequiredObject<T> : T;
+export type NonUndefined<T> = T extends undefined ? never : T;
+export interface _DeepRequiredArray<T> extends Array<DeepRequired<NonUndefined<T>>> {}
+export type _DeepRequiredObject<T> = { [P in keyof T]-?: DeepRequired<NonUndefined<T[P]>> };
+
+export type DeepPartial<T> = T extends Function ? T : T extends Array<infer U> ? _DeepPartialArray<U> : T extends object ? _DeepPartialObject<T> : T | undefined;
+export interface _DeepPartialArray<T> extends Array<DeepPartial<T>> {}
+export type _DeepPartialObject<T> = { [P in keyof T]?: DeepPartial<T[P]> };
 
 /**
  * get the type of elements inside of array, tuple or object of type T, that matches the given index type K
